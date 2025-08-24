b0c72aa879a999d2ca1a7ff9053871e99d90ab0a
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-08-24 19:13:34 +0530

    auto display flash messages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -77,16 +77,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	
-	// use PopString() method to retrieve the value for the "flash" key.
-	// PopString() also deletes the key and value from the session data
-	// so it acts like a one-time fetch. if there is no matching key in 
-	// the session data, this will return the empty string
-	flash := app.sessionManager.PopString(r.Context(), "flash")
 
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
-	data.Flash = flash
 
 	app.render(w, http.StatusOK, "view.html", data)
 }
